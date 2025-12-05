2a7a08f6d98e3ff6927da836ad5c30ad2c1af677
Mariana <mariana.iwaivk@gmail.com>
2025-12-05 08:16:18 -0300

    feat(header): add header component with basic layout

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.scss";
+import ThemeToggle from "../ThemeToggle";
+import DevFinderLogo from "../DevFinderLogo";
+
+const Header = ({ theme, onToggle }) => {
+    return (
+        <header className={styles.header}>
+            <DevFinderLogo />
+            <ThemeToggle theme={theme} onToggle={onToggle} />
+        </header>
+    );
+}
+
+export default Header;
