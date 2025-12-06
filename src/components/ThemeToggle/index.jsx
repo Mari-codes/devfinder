aca90d9dbb3121fab7875e5e468b972c4c0f56e7
Mariana <mariana.iwaivk@gmail.com>
2025-12-06 12:50:08 -0300

    feat(theme-toggle): add ThemeToggle component with styles

--- src/components/ThemeToggle/index.jsx
+++ src/components/ThemeToggle/index.jsx
@@ -1,16 +1,18 @@
-import styles from "./ThemeToggle.module.scss";
-import iconMoon from "../../assets/icons/icon-moon.svg"
-import iconSun from "../../assets/icons/icon-sun.svg";
+import styles from './ThemeToggle.module.scss';
+import iconMoon from '../../assets/icons/icon-moon.svg';
+import iconSun from '../../assets/icons/icon-sun.svg';
 
 const ThemeToggle = ({ theme, onToggle }) => {
-    const isLight = theme === "light";
+  const isLight = theme === 'light';
 
-    return (
-        <button className={styles["theme-toggle"]} onClick={onToggle}>
-            <span className={styles["theme-toggle__label"]}>{isLight ? "DARK" : "LIGHT"}</span>
-            <img src={isLight ? iconMoon : iconSun} alt="" />
-        </button>
-    );
-}
+  return (
+    <button className={styles['theme-toggle']} onClick={onToggle}>
+      <span className={styles['theme-toggle__label']}>
+        {isLight ? 'DARK' : 'LIGHT'}
+      </span>
+      <img src={isLight ? iconMoon : iconSun} alt="" />
+    </button>
+  );
+};
 
 export default ThemeToggle;
