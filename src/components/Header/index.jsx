f32cd06cae43d052d5496c31a158efd9407eba05
Mariana <mariana.iwaivk@gmail.com>
2025-12-07 03:36:14 -0300

    refactor(components): remove DevFinderLogo component and update Header styles and usage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
 import styles from './Header.module.scss';
 import ThemeToggle from '../ThemeToggle';
-import DevFinderLogo from '../DevFinderLogo';
+import DevFinderLogo from '../../assets/images/devfinder.svg?react';
 
 const Header = ({ theme, onToggle }) => {
   return (
     <header className={styles.header}>
-      <DevFinderLogo />
+      <DevFinderLogo className={styles.header__logo} />
       <ThemeToggle theme={theme} onToggle={onToggle} />
     </header>
   );
